0108d9229e5e672607df2334313e139560a8500d
priyank-py <ipriyank007@hotmail.com>
2019-11-22 15:14:56 +0530

    notifications added with link

--- budgets/context_processors.py
+++ budgets/context_processors.py
@@ -6,7 +6,10 @@ from datetime import date, timedelta
 def notification(request):
     
     seven_days = timedelta(days=7)
+    
 
-    # new_budget = Budget.objects.all().filter(date__gte=date.today()).filter(date__lte=date.today()+seven_days)
-    return {'new_budget': 45}
+    new_budget = Budget.objects.all().filter(date__gte=date.today()).filter(date__lte=date.today()+seven_days)
+    notification_no = new_budget.count()
+    
+    return {'new_budget': new_budget, 'notification_no': notification_no,}
 
